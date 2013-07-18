68fdbfd36ca7d7f063e95d2ea42721dd785cbf1b
Ubuntu <ubuntu@ip-172-31-39-76.us-west-2.compute.internal>
2013-07-18 20:29:20 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -3,6 +3,6 @@ var fs = require('fs');
 var port = process.env.PORT || 8080;
 var server = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-type": "text/html"});
-  response.end(fs.readFileSync("."+request.url));
+  response.end(fs.readFileSync("."+request.url+request.url.charAt(request.url.length-1) == '/' ? "index.html" :""));
 });
 server.listen(port);
