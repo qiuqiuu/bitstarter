97fbf1f0cb00e557ebbe87a474b98eb934923148
Ubuntu <ubuntu@ip-172-31-39-76.us-west-2.compute.internal>
2013-07-22 01:28:19 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -2,7 +2,12 @@ var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 8080;
 var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-type": "text/html"});
-  response.end(fs.readFileSync("."+request.url+(request.url.charAt(request.url.length-1) == '/' ? "index.html" :"")));
+  try {
+    var file = fs.readFileSync("."+request.url+(request.url.charAt(request.url.length-1) == '/' ? "index.html" :""));
+    response.writeHead(200, {"Content-type": "text/html"});
+    response.end(file);
+  }catch (err)
+  {response.writeHead(404, {"Content-type":"text/html"});
+   response.end("404 Not Found");}
 });
 server.listen(port);
